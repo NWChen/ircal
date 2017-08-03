e37de38fb78ef6b46755a5f964f3e89ac7ccb852
Neil Chen <neilwchen@gmail.com>
2017-08-03 14:46:41 -0700

    Wrap all FakeSerial mock functions in connection check. FakeSerial inherits from real Serial class.

--- tests/fake_serial.py
+++ tests/fake_serial.py
@@ -1,6 +1,8 @@
 from serial import Serial, SerialException
 from itertools import chain
 from random import randint
+from functools import wraps
+import re
 
 class Responder():
     """Generates mock KT/CT output given environment information."""
@@ -15,18 +17,12 @@ class Responder():
             'calibration_factor': 2.0,
         }
         self.language = {
-            '^CAL ?$': (lambda settings, device_values:
-                            device_values['calibration_factor']),
-            '^CAL (\d+[.]\d*))$': (lambda settings, device_values: 
-                            device_values.update({'calibration_factor': float(settings.group()[0])})),
-            '^TEMP$': (lambda settings, device_values:
-                            device_values['temp']),
-            '^RAD$': (lambda settings, device_values:
-                            device_values['rad']),
-            '^UNIT ?$': (lambda settings, device_values:
-                            device_values['unit']),
-            '^UNIT ([K|C|F])$': (lambda settings, device_values:
-                            device_values.update({'unit': settings.group()[0]}))
+            '^CAL ?$': (lambda settings, device_values: device_values['calibration_factor']),
+            '^CAL (\d+[.]\d*))$': (lambda settings, device_values: device_values.update({'calibration_factor': float(settings.group()[0])})),
+            '^TEMP$': (lambda settings, device_values: device_values['temp']),
+            '^RAD$': (lambda settings, device_values: device_values['rad']),
+            '^UNIT ?$': (lambda settings, device_values: device_values['unit']),
+            '^UNIT ([K|C|F])$': (lambda settings, device_values: device_values.update({'unit': settings.group()[0]}))
         }
         self.query = []
 
@@ -63,9 +59,9 @@ class Responder():
             settings = re.match(command, self.query)
             if settings:
                 return response(settings, self.device_values)
-        raise SerialException("ERROR 19: CAN'T DO IT") 
+        raise SerialException("ERROR 19: CAN'T DO IT")
 
-class FakeSerial():#Serial):
+class FakeSerial(Serial):
     """Simulates generic serial device behavior.
        This class does not mock serial.tools.list_ports.comports behavior,
        since that function call is expected to function regardless of whether a KT/CT is plugged in.
@@ -76,21 +72,28 @@ class FakeSerial():#Serial):
             :param port: Serial port, we don't really care what format.
             :type port: String
         """
+        super(FakeSerial, self).__init__()
         self.name = self.port = port
         self.responder = Responder() # Delegate generating KT/CT-like responses to a separate object.
                                      # Whenever mock KT/CT command output is desired, a call to self.responder should be made.
         self.output_buffer = self.input_buffer = []
+        self.baudrate = baudrate
+        self.timeout = timeout
         self.is_open = True
 
-    def __getattribute__(self, attr):
+    def check_connection(func):
         """Checks whether this connection is open prior to any member/function call.
-           This is less invasive than checking for self.is_open in every function call.
+           This is less invasive than checking for self._is_open in every function call.
         """
-        if attr != 'is_open': # self.is_open is the only call that functions properly regardless of connection state.
+        @wraps(func)
+        def wrapper(self, *args, **kwargs):
             if not self.is_open:
                 raise SerialException("Cannot write, serial connection is not open.")
-        return object.__getattribute__(self, attr)
+            else:
+                return func(self, *args, **kwargs)
+        return wrapper
 
+    @check_connection
     def readline(self):
         """Provide a mock response from the serial object.
             :returns: Fake response corresponding to the input query specified by a write().
@@ -102,6 +105,7 @@ class FakeSerial():#Serial):
             return tokens
         sleep(self._timeout()) # Just like a real serial connection, block until EOF/EOL or timeout elapsed. We could choose to include the 1/10 failure rate here, but I chose not to.
 
+    @check_connection
     def write(self, query):
         """Accept a query and generate potential responses, to be consumed in readline().
             :param query: Input string usually sent to a serial object.
@@ -113,12 +117,14 @@ class FakeSerial():#Serial):
         else:
             raise SerialException("Invalid input query.")
  
+    @check_connection
     def open(self):
         """Pretends to open a serial connection.
         """
-        self.is_open = True
+        self._is_open = True
 
+    @check_connection
     def close(self):
         """Pretends to close a serial connection.
         """
-        self.is_open = False
+        self._is_open = False

0d6ef5b543faa5b2082bae221720443e560d5141
Neil Chen <neilwchen@gmail.com>
2017-08-03 17:22:19 -0700

    Run all test suites in tests/ on __init__, mostly for Travis.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,15 @@
 import sys
+import test_fake_serial
+import unittest
 from fake_serial import FakeSerial
 
-def main():
-    fs = FakeSerial(1) 
-    return 0
-
 if __name__ == '__main__':
-    sys.exit(main())
+    try:
+        all_tests = []
+        all_tests.append(unittest.TestSuite([test_fake_serial.get_tests()]))
+        
+        for test in all_tests:
+            unittest.TextTestRunner().run(test)
+        sys.exit(0)
+    except:
+        sys.exit(1)

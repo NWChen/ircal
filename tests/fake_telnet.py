dd09e7f707801850bf8696b4530052fbaf27087e
Neil Chen <neilwchen@gmail.com>
2017-08-04 15:33:43 -0700

    Add dummy telnet server to function behind Telnet calls

--- tests/fake_telnet.py
+++ tests/fake_telnet.py
@@ -1,4 +1,41 @@
-from telnetlib import Telnet
+import socket
+import threading
 
-class FakeTelnet(Telnet):
-    pass
+class daemon(threading.Thread):
+    """A fake telnet server emulating a blackbody emitter controller."""
+
+    def __init__(self, (socket, addr)):
+        threading.Thread.__init__(self)
+        self.socket = socket
+        self.addr = addr
+        self.device_values = {
+            'temp': '25.0'
+        }
+        self.language = {
+            'DA(\d+[.]*\d*)\r\n': (lambda settings: self.device_values.update({'setpoint': settings.group(1)})),
+            'M2\r\n': (lambda settings: self.device_values['temp'])
+        }
+
+    def handle(self, data):
+        for command, response in self.language.items():
+            settings = re.match(command, data)
+            if settings:
+                res = response(settings)
+                if res:
+                    return res + '\r\n'
+        return ''
+
+    def run(self):
+        self.socket.send("SUCCESSFULLY CONNECTED.\n")
+        while(True):
+            data = self.socket.recv(1024)
+            response = handle(data)
+            self.socket.send(response)
+        self.socket.close()
+
+def start():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('', 9999))
+    sock.listen(1)
+    lock = threading.Lock()
+    daemon(sock.accept()).start()

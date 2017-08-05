573daa276be301265c05c147f88553c5bbbba0de
Neil Chen <neilwchen@gmail.com>
2017-08-04 20:47:54 -0700

    Update temperature in parallel with Daemon receiving through socket.

--- tests/fake_telnet.py
+++ tests/fake_telnet.py
@@ -1,6 +1,7 @@
 import re
 import socket
 import threading
+from time import sleep
 
 class Daemon(threading.Thread):
     """A fake telnet server emulating a blackbody emitter controller."""
@@ -10,14 +11,32 @@ class Daemon(threading.Thread):
         self.socket = socket
         self.addr = addr
         self.device_values = {
+            'old_setpoint': '25.0',
             'setpoint': '25.0',
             'temp': '25.0'
         }
         self.language = {
-            'DA(\d+[.]*\d*)\r\n': (lambda settings: self.device_values.update({'setpoint': settings.group(1)})),
+            'DA(\d+[.]*\d*)\r\n': (lambda settings: self.begin_updating(settings)),
             'M1\r\n': (lambda settings: self.device_values['setpoint']),
             'M2\r\n': (lambda settings: self.device_values['temp'])
         }
+        self._timeout = 0.2
+
+    def update(self):
+        temp = float(self.device_values.get('temp'))
+        setpoint = float(self.device_values.get('setpoint'))
+        while temp < setpoint - 0.1 or temp > setpoint + 0.1:
+            temp = float(self.device_values.get('temp'))
+            old_setpoint = float(self.device_values.get('old_setpoint', '0.0'))
+            setpoint = float(self.device_values.get('setpoint', old_setpoint))
+            temp += (setpoint - old_setpoint) / temp
+            self.device_values.update({'temp': str(temp)})
+            sleep(self._timeout)
+        return
+
+    def begin_updating(self, settings):
+        self.device_values.update({'setpoint': settings.group(1)})
+        threading.Thread(target=self.update).start() 
 
     def handle(self, data):
         for command, response in self.language.items():

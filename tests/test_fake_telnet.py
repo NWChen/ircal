f295f00b7476f375fc20d7d741c1ba2013c7f064
Neil Chen <neilwchen@gmail.com>
2017-08-06 15:40:36 -0700

    FakeTelnet test case passes but does not exit

--- tests/test_fake_telnet.py
+++ tests/test_fake_telnet.py
@@ -1,28 +1,40 @@
 import unittest
-import threading
 import sys
-import fake_telnet
 import time
+import threading
+from subprocess import call
 from telnetlib import Telnet
 
-t = threading.Thread(target=fake_telnet.start)
+class NonBlockingDaemon(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        threading.Thread(target=self._begin_server).start()
+        self.shutdown = False
+
+    def _begin_server(self):
+        proc = call(["python", "fake_telnet.py"])
+        while not self.shutdown:
+            pass
+        proc.terminate()
+        sys.exit(0)
 
 class FakeTelnetTestCase(unittest.TestCase):
 
     def setUp(self):
+        self.nbd = NonBlockingDaemon()
+        time.sleep(1)
         self.tn = Telnet() 
         try:
-            global t
-            t.start()
             self.tn = Telnet('localhost', 9999)
             time.sleep(1)
             self.tn.read_very_eager() 
-        except Exception:
-            pass
-        time.sleep(1)
+        except Exception as e:
+            print 'unreasonable connection attempt'
+            print e
 
     def tearDown(self):
         self.tn.close()
+        self.nbd.shutdown = True
 
     def test_initialization(self):
         self.tn.write('M2\r\n')
@@ -33,17 +45,16 @@ class FakeTelnetTestCase(unittest.TestCase):
         time.sleep(1)
         self.assertEqual(self.tn.read_very_eager(), '25.0\r\n')
 
-    '''
-    def test_temp_change(self):
-        self.tn.write('DA32.0\r\n')
-        self.tn.write('M1\r\n')
-        time.sleep(1)
-        self.assertEqual(self.tn.read_very_eager(), '32.0\r\n')
-    '''
+    #def test_temp_change(self):
+    #    self.tn.write('DA32.0\r\n')
+    #    self.tn.write('M1\r\n')
+    #    time.sleep(1)
+    #    self.assertEqual(self.tn.read_very_eager(), '32.0\r\n')
 
 def get_tests():
     tests = ['test_initialization']#, 'test_temp_change']
     return unittest.TestSuite(map(FakeTelnetTestCase, tests))
 
 if __name__ == '__main__':
-    sys.exit(unittest.main())
+    unittest.main()
+    sys.exit(0)

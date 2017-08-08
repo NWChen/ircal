cb3158a7776b809bfdf3f1ceede2e9faac65fe65
Neil Chen <neilwchen@gmail.com>
2017-08-07 23:27:56 -0700

    Add docstring for read_data().

--- src/app.py
+++ src/app.py
@@ -5,17 +5,26 @@ from bokeh.layouts import gridplot
 from bokeh.plotting import figure, show, output_file
 
 def read_data(filename):
+    """Converts a tab-separated data file into a pandas DataFrame. Any timestamps are converted into datetime objects.
+        :param filename: Absolute or relative path to data file
+        :type filename: String
+        :returns: DataFrame containing datetime objects and corresponding rows of ata
+        :rtype: pandas.DataFrame
+    """
     df = pd.read_csv(filename, sep='\t', lineterminator='\n')
     df['TIME'] = pd.to_datetime(df['TIME'], format='%m/%d/%Y %H:%M:%S.%f')
     df.columns = df.columns.str.replace('\r', '')
     return df
 
 def generate_graph(df):
-    pass 
+    fig = figure(x_axis_type='datetime')
+    fig.line(df['TIME'], df['KT SEA REF'])
+    return fig
 
 def main():
     """Application entry point."""
-    pass
+    filename = sys.argv[1]
+    show(generate_graph(read_data(filename)))
 
 if __name__ == '__main__':
     sys.exit(main())

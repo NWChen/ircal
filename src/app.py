f0a5d036bd83fd49a739a91fb01d3ac1fb16793e
Neil Chen <neilwchen@gmail.com>
2017-08-07 23:06:18 -0700

    Add sample data. Use pandas to read in tab-separated data.

--- src/app.py
+++ src/app.py
@@ -1,4 +1,17 @@
 import sys
+import pandas as pd
+
+from bokeh.layouts import gridplot
+from bokeh.plotting import figure, show, output_file
+
+def read_data(filename):
+    df = pd.read_csv(filename, sep='\t', lineterminator='\n')
+    df['TIME'] = pd.to_datetime(df['TIME'], format='%m/%d/%Y %H:%M:%S.%f')
+    df.columns = df.columns.str.replace('\r', '')
+    return df
+
+def generate_graph(df):
+    pass 
 
 def main():
     """Application entry point."""

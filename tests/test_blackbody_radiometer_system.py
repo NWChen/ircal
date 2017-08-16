09060070dc30077926e9509875144ec8ac7ff925
Neil Chen <neilwchen@gmail.com>
2017-08-16 13:03:17 -0700

    Blackbody/radiometer system only tests blackbody response

--- tests/test_blackbody_radiometer_system.py
+++ tests/test_blackbody_radiometer_system.py
@@ -1,10 +1,40 @@
 import unittest
+import time
+from fake_serial import FakeSerial
+from test_fake_telnet import NonBlockingDaemon
+
+import sys
+sys.path.append('../src') # bring in Radiometer, Blackbody classes from ../src
+from radiometer import Radiometer
+from blackbody import Blackbody
 
 class FakeSystemTestCase(unittest.TestCase):
 
-    def SetUp(self):
+    def setUp(self):
+        try:
+            self.nbd = NonBlockingDaemon()
+        except Exception as e:
+            print e
+            sys.exit(0)
+        time.sleep(1)
+        FAKE_PORT = 1
+        self.serial = FakeSerial(port=FAKE_PORT)
+        self.radiometer = Radiometer(self.serial)
+        self.blackbody = Blackbody(addr='localhost', port=9999)
 
     def tearDown(self):
+        self.blackbody.close()
+        if self.serial.is_open:
+            self.serial.close()
+
+    def test_blackbody_updates(self):
+        self.blackbody.set_temperature(36.5)
+        current_temp = self.blackbody.get_temperature()
+        while(current_temp < 36.1):
+            time.sleep(0.2)
+            current_temp = self.blackbody.get_temperature()
+            print current_temp
+        return True
 
 if __name__ == '__main__':
     sys.exit(unittest.main())
